b56f8740f03570ce5dc1738f20489f9930571d45
s-a-b-y-a <159752874+s-a-b-y-a@users.noreply.github.com>
2024-06-16 10:05:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function updateSongTime() {
 async function getSongs(folder) {
     songs = []
     currfolder = folder
-    let a = await fetch(`http://127.0.0.1:3000/spotify_clone/${folder}/`);
+    let a = await fetch(`/spotify_clone/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
